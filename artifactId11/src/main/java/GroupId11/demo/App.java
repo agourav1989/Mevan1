eefeaaf5055b9949d414b4c39816825edad8c93b
Gourav Arora <gourav.arora@nagarro.com>
2018-10-09 14:08:19 +0530

    First commit

---
+++ artifactId11/src/main/java/GroupId11/demo/App.java
@@ -0,0 +1,13 @@
+package GroupId11.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
