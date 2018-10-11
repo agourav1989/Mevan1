b3d680e27f5ab28e3e40c63f662fceea3f970f69
agourav1989 <43431316+agourav1989@users.noreply.github.com>
2018-10-11 19:14:09 +0530

    Update App.java

--- src/main/java/GroupId11/demo/App.java
+++ src/main/java/GroupId11/demo/App.java
@@ -6,8 +6,16 @@ package GroupId11.demo;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World2!" );
-    }
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
 }
